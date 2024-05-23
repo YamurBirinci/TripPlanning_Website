360a8664fe31a062b4cef2697e8d893e40dedb40
helin-arslan <99952363+helin-arslan@users.noreply.github.com>
2024-05-23 15:17:38 +0300

    Delete src directory

--- src/main/java/com/group09/travelPlanner/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group09.travelPlanner.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-//import com.group09.travelPlanner.dao.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
